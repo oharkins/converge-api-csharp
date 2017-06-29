e4796c6825852cbfd91940bdcc2888d7b33f221e
Alex Estrada <alex@cucsoft.com>
2017-06-29 17:17:16 -0600

    changed to Framework 5.0

--- ConvergAPI/testClass.cs
+++ ConvergAPI/testClass.cs
@@ -7,23 +7,23 @@ namespace oharkins.ConvergAPI
     [TestFixture]
     public class testClass
     {   
-
         [Test]
         public void GetToken()
         {
-
 			TokenClass.TokenRequest TR = new TokenClass.TokenRequest()
 			{
-				ssl_merchant_id = "008092",
-				ssl_user_id = "webpage",
-				ssl_pin = "UZ0I5F",
-				ssl_test_mode = true,
-				ssl_card_number = "4111111111111111",
-				ssl_exp_date = "1225",
-				ssl_first_name = "Odis",
-				ssl_last_name = "Harkins",
-				ssl_cvv2cvc2 = "123"
-			};
+                ssl_merchant_id = "008092",
+                ssl_user_id = "webpage",
+                ssl_pin = "UZ0I5F",
+                ssl_test_mode = true,
+                ssl_card_number = "4124939999999990",
+                ssl_exp_date = "1225",
+                ssl_first_name = "Odis",
+                ssl_last_name = "Harkins",
+                ssl_cvv2cvc2 = "123",
+                ssl_avs_zip = "12345",
+                ssl_verify = "Y"
+            };
 
 			var responce = new TokenClass.TokenResponse();
 
